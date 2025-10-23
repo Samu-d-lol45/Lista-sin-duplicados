dad5658762173f0f43ddbc101d01853c5ca2fda6
Samu-d-lol45 <smldvdmartine@gmail.com>
2025-10-23 19:49:02 -0400

    Update ELIMINAR_DUPLICADOS.pl

--- SRC/ELIMINAR_DUPLICADOS.pl
+++ SRC/ELIMINAR_DUPLICADOS.pl
@@ -12,32 +12,6 @@ eliminar_duplicados([Cabeza|Cola], ListaSinDuplicados) :-
     ;   ListaSinDuplicados = [Cabeza|ListaTemp]
     ).
 
-% Versión alternativa usando acumulador (más eficiente)
-eliminar_duplicados_v2(ListaOriginal, ListaSinDuplicados) :-
-    eliminar_duplicados_v2(ListaOriginal, [], ListaSinDuplicadosReversa),
-    reverse(ListaSinDuplicadosReversa, ListaSinDuplicados).
+ 
 
-eliminar_duplicados_v2([], Acumulador, Acumulador).
-
-eliminar_duplicados_v2([Cabeza|Cola], Acumulador, Resultado) :-
-    (   member(Cabeza, Acumulador)
-    ->  eliminar_duplicados_v2(Cola, Acumulador, Resultado)
-    ;   eliminar_duplicados_v2(Cola, [Cabeza|Acumulador], Resultado)
-    ).
-
-% Ejemplos de uso para testing
-:- begin_tests(eliminar_duplicados).
-
-test(elimina_duplicados_lista_vacia) :-
-    eliminar_duplicados([], []).
-
-test(elimina_duplicados_sin_duplicados) :-
-    eliminar_duplicados([a,b,c], [a,b,c]).
-
-test(elimina_duplicados_con_duplicados) :-
-    eliminar_duplicados([a,b,c,a,d,b,e], [a,b,c,d,e]).
-
-test(mantiene_orden_relativo) :-
-    eliminar_duplicados([1,2,3,2,1], [1,2,3]).
-
-:- end_tests(eliminar_duplicados).
+ 
